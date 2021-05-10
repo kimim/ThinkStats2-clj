30d3ccecf92921e4075e44babce7407b858de7a4
kimim <kimi.im@outlook.com>
2021-05-10 13:02:01 +0800

    add histogram plot based on XChart

---
+++ src/thinkstats2_clj/chart.clj
@@ -0,0 +1,17 @@
+(ns thinkstats2-clj.chart
+  (:import
+   (org.knowm.xchart XYChart
+                     CategoryChart
+                     PieChart
+                     VectorGraphicsEncoder
+                     VectorGraphicsEncoder$VectorGraphicsFormat)))
+
+(defn histogram
+  "Plot a histogram chart"
+  [xs ys & {:keys [filename title legend width height]
+            :or {title "histogram" legend "y(x)" width 500 height 400}}]
+  (let [xy (CategoryChart. width height)]
+    (.setTitle xy title)
+    (.addSeries xy legend xs ys)
+    (VectorGraphicsEncoder/saveVectorGraphic
+     xy filename VectorGraphicsEncoder$VectorGraphicsFormat/PDF)))
